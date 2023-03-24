0ade3fde1eb5e7a8b1e69b09293c8cda639e1c2f
Ahmed Shuja Abbasi <shujaabbasi46@gmail.com>
2023-03-24 16:03:16 +0500

    initial commit

---
+++ back-end/Tweet.py
@@ -0,0 +1,13 @@
+from hate_speech_type import hate_speech_type
+
+# stores a single tweet against prediction
+class Tweet():
+    def __init__(self, tweet: str="",type: hate_speech_type=hate_speech_type.none ) -> None:
+        self.tweet = tweet
+        self.type = type
+    
+    def set_tweet(self, tweet: str):
+        self.tweet = tweet
+
+    def set_type(self, type: hate_speech_type):
+        self.type = type
